305373ae8abecc81b9280ac23acdf351f47eface
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-31 18:01:50 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,9 @@
 class Vehicle
   attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+    
 end
